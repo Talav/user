1d359e3f34516f921ebc7ae6b9c851f54413bcdb
GitHub <action@github.com>
2022-05-24 20:34:36 +0000

    [User] Fix create DTO constructor

--- src/Message/Dto/CreateUserDto.php
+++ src/Message/Dto/CreateUserDto.php
@@ -6,15 +6,23 @@ namespace Talav\Component\User\Message\Dto;
 
 use Talav\Component\Resource\Model\DomainEventInterface;
 
+// @todo, find a better approach to create DTO
 final class CreateUserDto implements DomainEventInterface
 {
+    public bool $active = true;
+    public ?string $firstName = null;
+    public ?string $lastName = null;
+
     public function __construct(
         public string $username,
         public string $email,
         public string $password,
-        public bool $active = true,
-        public ?string $firstName = null,
-        public ?string $lastName = null
+        bool $active = true,
+        ?string $firstName = null,
+        ?string $lastName = null
     ) {
+        $this->active = $active;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
     }
 }
